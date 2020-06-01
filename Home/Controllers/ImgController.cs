1944e9ecf46fc127b57e45bf1355db699482cb24
ne7atuve <ne7atuve@gmail.com>
2020-06-01 20:21:02 +0500

    Добавьте файлы проекта.

---
+++ Home/Controllers/ImgController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Hosting;
+using Home.Models;
+using Home.Controllers;
+
+namespace Home.Controllers
+{
+    public class ImgController : Controller
+    {
+        private readonly IWebHostEnvironment _iweb;
+        
+        public ImgController(IWebHostEnvironment iweb)
+        {
+            _iweb = iweb;
+        }
+
+
+
+        public IActionResult Index()
+        {
+            ImageClass ic = new ImageClass();
+            var displayimg = Path.Combine(_iweb.WebRootPath, "UploadFile");
+            DirectoryInfo di = new DirectoryInfo(displayimg);
+            FileInfo[] fileinfo = di.GetFiles();
+            ic.Fileimage = fileinfo;
+            return View(ic);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(IFormFile imgfile)
+        {
+            string ext = Path.GetExtension(imgfile.FileName);
+            if (ext == ".jpg" || ext == ".JPG" || ext == ".pdf" || ext == ".mp3" || ext == ".docx" || ext == ".doc")
+            {
+                var imgsave = Path.Combine(_iweb.WebRootPath, "UploadFile", imgfile.FileName);
+                var stream = new FileStream(imgsave, FileMode.Create);
+                await imgfile.CopyToAsync(stream);
+                stream.Close();
+            }
+            return RedirectToAction("Index");
+        }
+
+
+
+
+
+    }
+}
---
+++ Home/Models/ImageClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Home.Models
+{
+    public class ImageClass
+    {
+        public FileInfo[] Fileimage { get; set; }
+    }
+}
